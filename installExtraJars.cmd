08cb23031b92e26bc42bd40fe52a6594570273e5
epuzanov <epuzanov@users.noreply.github.com>
2023-02-03 19:51:53 +0100

    add gsrs-module-substance-core with JoseCrypto support

--- installExtraJars.cmd
+++ installExtraJars.cmd
@@ -1,1 +1,2 @@
 mvnw.cmd install:install-file -Dfile=extraJars/gsrs-ep-substance-extension-3.0.3-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/gsrs-module-substances-core-3.0.3-SNAPSHOT.jar
